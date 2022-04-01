2a226fe88904c741b7d24e0e3d3660c192afa520
MaferOrdonez1 <90990182+MaferOrdonez1@users.noreply.github.com>
2022-03-31 20:45:13 -0600

    Add files via upload

--- Nodo.java
+++ Nodo.java
@@ -2,14 +2,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package suma.de.colas;
+package listas_pilas;
 
 /**
  *
  * @author mafer
  */
 public class Nodo {
-    int dato;
+    String dato;
     Nodo siguiente;
-   
+    
+    public Nodo(String dato){
+        this.dato = dato;
+        siguiente = null;
+    
+    }
 }
